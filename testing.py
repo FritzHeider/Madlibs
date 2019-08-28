50d6dbf6e0d1d7a27c12a831a5bf377a272e643d
FritzHeider <blueloke@gmail.com>
2019-08-28 16:02:56 -0700

    This is the absolute barebones beginnings of the madlibs game

---
+++ testing.py
@@ -0,0 +1,9 @@
+print ("Hello! Welcome to Mad Fritz's Lib Game!")
+print ("In order to play the game i need you to input some parts of speech for me")
+
+
+
+a = input('please enter an adjective: ')
+
+a1 = input('please enter a second adjective: ')
+a2 = input('please enter an just one more adjective: ')

abb625e0173ecbeaef8bf6d0e898eee5f9bbf8e4
FritzHeider <blueloke@gmail.com>
2019-09-02 00:34:09 -0700

    Alright the addition of a madlib script in a seperate file as a potential function or to combine with whole later

---
+++ monkey.py
@@ -0,0 +1,11 @@
+##damn i cant remember if i can use arrays or lists as parameters in functions, why woiuldnt i right? 
+
+
+
+def monkeylib(nouns, adjec, verb):
+    print("The day I saw the Monkey King I " + verb[0] + " It was one of the most interesting days of the year.")
+    print("I still cannot believe my " + nouns[0] + "I had the " + verb[1] "running through my body for over a week.")
+    print("The Monkey King is one of the largest " + nouns[1] "I had seen in person. The funniest thing was when he" + adjec[0])
+    print(verb[2] + "in a huge pile of " + nouns[2] + "After he did that he played chess on his brothers" + nouns )
+    print("and combed his " + adjec[2] + " hair with a comb made out of fish bones")
+    print("Later that same day, i saw the monkey king dance" + verbs[3] + " in front of an audience of" + nouns[3])
